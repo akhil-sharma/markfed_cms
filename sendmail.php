47216310a24b9b9786cd24ad2cdd594cc85d4528
Anubhav Gupta <anubhav.gupta97@gmail.com>
2017-10-30 14:39:01 +0530

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -8,7 +8,7 @@
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-$headers .= "From: successive.testing@gmail.com" . "\r\n" .
+$headers .= "From: anubhav.gupta3210@gmail.com" . "\r\n" .
 "Reply-To: successive.testing@gmail.com" . "\r\n" .
 "X-Mailer: PHP/" . phpversion();
 
